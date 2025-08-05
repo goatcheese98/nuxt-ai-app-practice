565fdf5bb9162c2042c7208e5ac2547379580f2d
Rohan Jasani <jasani.rohan@gmail.com>
2025-08-06 00:48:37 +0530

    Checkpoint 1: Project Setup (with test logic for husky)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  future: { compatibilityVersion: 4, },
-  CSS: ['~/@/assets/css/main.css'],
-  modules: ['@nuxt/ui', '@nuxt/eslint']
-})
+  future: { compatibilityVersion: 4 },
+  css: ["~/@/assets/css/main.css"],
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
+});
